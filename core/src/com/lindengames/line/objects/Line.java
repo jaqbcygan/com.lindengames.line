852057577087b58c107af821568c5656fb54e100
jaqbcygan <cygan6914@gmail.com>
2016-07-25 18:00:12 +0200

    Initial commit

---
+++ core/src/com/lindengames/line/objects/Line.java
@@ -0,0 +1,13 @@
+package com.lindengames.line.objects;
+
+import com.badlogic.gdx.physics.box2d.World;
+
+public class Line {
+
+    private World world;
+
+    public Line(World world){
+        this.world = world;
+    }
+}
+
